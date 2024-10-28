be821d3f7412e078b9026650da617c267e77fd30
Ajeet Kumar <ajeet62068bca@gmail.com>
2024-10-28 16:56:37 +0530

    added session providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import Image from "next/image";
 import { Buttonbox } from "@/components/button";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
   return (
     <div className="text-3xl">
       hi there
       <Buttonbox/>
+      <button onClick={()=>{
+        signIn();
+      }}>signin</button>
     </div>
   );
 }

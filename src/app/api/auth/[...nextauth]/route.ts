6e567fd9b6961b0c61b3b07fe37d9d34e71e0566
Ajeet Kumar <ajeet62068bca@gmail.com>
2024-10-28 12:29:09 +0530

    added prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export function GET(req: NextRequest, params: any){
-    console.log(params)
-    return NextResponse.json({
-        message: "hello man"
-    })
-}
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any) {
+        return {
+          id: "user2",
+        };
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
